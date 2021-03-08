adb0561ac6cf234a42fb646735299459ad5c74e2
pnv13 <ivanpakhotnykh@mail.ru>
2021-03-08 22:39:30 +0300

    init: start and end reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let newNumber = Math.abs(n) + '';
+    let revNumber = newNumber.split("").reverse().join("");
+    return +revNumber;
+};
